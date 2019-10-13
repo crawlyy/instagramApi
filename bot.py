0557959fcea5305a9ee337d637b884e61cb9bd5e
crawlyy <48304706+crawlyy@users.noreply.github.com>
2019-10-13 08:10:16 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from instaApi import InstagramAPI
+from random import randint
+import time
+import sys
+
+
+
+USERNAME = "your instagram username here"
+PASSWORD = "your instagram password here"
+
+bot = InstagramAPI(USERNAME,PASSWORD)
+bot.login()
+bot.getUsernameInfo('cats')
+print bot.LastJson
